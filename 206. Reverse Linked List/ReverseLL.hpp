ad47f9a46c620de85b9f54f78903b1fab8a769b6
methumk <methum.cg@gmail.com>
2021-04-03 09:10:25 -0700

    adding reverse linked list

---
+++ 206. Reverse Linked List/ReverseLL.hpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* h = head, *hold = NULL;
+        
+        while(h != NULL){
+            ListNode* tmp = h;
+            h = h->next;
+            tmp->next = hold;
+            hold = tmp;
+        }
+        
+        return hold;
+    }
+};
